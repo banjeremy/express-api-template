127f69ed27e22d38ca58e3f5b501022b337fdf4d
Jeremy Jones <banjeremy@gmail.com>
2016-02-28 18:08:41 -0600

    use bcrypt for hashing passwords

--- app/routes/authenticate/index.js
+++ app/routes/authenticate/index.js
@@ -1,33 +1,45 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var router = express.Router();
 var app = express();
 var User = require('../../models/user');
 var jwt = require('jsonwebtoken');
 var config = require('../../config');
 
+var fail = function(req, res) {
+  res.status(401).json({
+    success: false,
+    message: 'Authentication failed.'
+  });
+}
+
 router.post('/', (req, res) => {
   User.where({
-    username: req.body.username,
-    password: req.body.password
+    username: req.body.username
   }).fetch()
     .then((user) => {
       if (user) {
-        var token = jwt.sign(user.toJSON(), config.secret, {
-          expiresInMinutes: 1440 // 24 hours
-        });
+        var storedHash = user.get('password');
+        
+        bcrypt.compare(req.body.password, storedHash, (err, matches) => {
+          if (matches) {
+            var token = jwt.sign(user.toJSON(), config.secret, {
+              expiresInMinutes: 1440 // 24 hours
+            });
 
-        res.json({
-          success: true,
-          message: 'Enjoy your token',
-          data: {
-            token
+            res.json({
+              success: true,
+              message: 'Enjoy your token',
+              data: {
+                token
+              }
+            });
+          } else {
+            fail(req, res);
           }
         });
       } else {
-        res.status(401).json({
-          success: false,
-          message: 'Authentication failed.'
-        });
+        fail(req, res);
       }
     });
 });
--- seeds/users_seed.js
+++ seeds/users_seed.js
@@ -5,9 +5,8 @@ exports.seed = function(knex, Promise) {
     knex('users').del(),
 
     // Inserts seed entries
-    knex('users').insert({id: 1, username: 'user 1', email: 'user1@email.com', password: 'abc123'}),
-    knex('users').insert({id: 2, username: 'user 2', email: 'user2@email.com', password: 'abc123'}),
-    knex('users').insert({id: 3, username: 'user 3', email: 'user3@email.com', password: 'abc123'}),
-    knex('users').insert({id: 4, username: 'user 4', email: 'user4@email.com', password: 'abc123'})
+    knex('users').insert({id: 1, username: 'user 1', email: 'user1@email.com', password: '$2a$10$44hw/Xw6XFoan5o58doHy.HGJD4lyp1JKZNsUvTpryQISbhIABkxK'}),
+    knex('users').insert({id: 2, username: 'user 2', email: 'user2@email.com', password: '$2a$10$j/Mt07xybhi96zTjjxfm7On8GVPsSr1rJv9Jw6eCTq5Fm32WkbGzy'}),
+    knex('users').insert({id: 3, username: 'user 3', email: 'user3@email.com', password: '$2a$10$6qkmUda/PFjDEZmp0WxoQe3Pz.fLYtx4hsZBsxXJ3NDgCZErbEJT.'})
   );
 };
